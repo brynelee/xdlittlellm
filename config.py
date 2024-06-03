4a2f95ae543a59c801ea2113078055811e718426
Bryne Lee <bryne_lxd@sina.com>
2024-06-04 00:23:08 +0800

    added gpt.py

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 VOCAB_SIZE=500
 MAX_SEQ_LEN=2000     # GPT模型输入限制
 
+# transformer
+GPT_DIM=384
+GPT_HEAD=6
+GPT_FF=1024
+GPT_BLOCKS=6
+
 # special tokens
 IM_START='<|im_start|>'
 IM_END='<|im_end|>'
